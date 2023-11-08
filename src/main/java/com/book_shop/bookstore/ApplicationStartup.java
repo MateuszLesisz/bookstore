690f7ff938bf3c6cae6ed36cd62725eef34b6eac
Mateusz Lesisz <m.lesisz97@wp.pl>
2023-11-08 11:19:16 +0100

    inject values using @Value + update .gitignore

--- src/main/java/com/book_shop/bookstore/ApplicationStartup.java
+++ src/main/java/com/book_shop/bookstore/ApplicationStartup.java
@@ -2,21 +2,33 @@ package com.book_shop.bookstore;
 
 import com.book_shop.bookstore.catalog.application.CatalogController;
 import com.book_shop.bookstore.catalog.domain.Book;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-@AllArgsConstructor
 public class ApplicationStartup implements CommandLineRunner {
 
     private final CatalogController catalogController;
+    private final String title;
+    private final Long limit;
+
+    public ApplicationStartup(
+            CatalogController catalogController,
+            @Value("${book.catalog.query}") String title,
+            @Value("${book.catalog.limit}") Long limit
+    ) {
+        this.catalogController = catalogController;
+        this.title = title;
+        this.limit = limit;
+    }
+
 
     @Override
     public void run(String... args) {
-        List<Book> books = catalogController.findByTitle("Pan Tadeusz");
-        books.forEach(System.out::println);
+        List<Book> books = catalogController.findByTitle(title);
+        books.stream().limit(limit).forEach(System.out::println);
     }
 }
