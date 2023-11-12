7f5d14c45b729a2f957c67cae43c4a46cea7adb4
Mateusz Lesisz <m.lesisz97@wp.pl>
2023-11-12 17:22:33 +0100

    create first controller

---
+++ src/main/java/com/book_shop/bookstore/catalog/web/CatalogController.java
@@ -0,0 +1,23 @@
+package com.book_shop.bookstore.catalog.web;
+
+import com.book_shop.bookstore.catalog.application.port.CatalogUseCase;
+import com.book_shop.bookstore.catalog.domain.Book;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/catalog")
+@RestController
+@AllArgsConstructor
+public class CatalogController {
+
+    private final CatalogUseCase catalog;
+
+    @GetMapping
+    public List<Book> findAll() {
+        return catalog.findAll();
+    }
+}
