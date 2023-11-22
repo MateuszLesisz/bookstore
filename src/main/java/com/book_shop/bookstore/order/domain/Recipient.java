ae0d2e95844ad0596bb92de3c972695fb030f2ff
Mateusz Lesisz <m.lesisz97@wp.pl>
2023-11-22 17:34:49 +0100

    update Recipient

--- src/main/java/com/book_shop/bookstore/order/domain/Recipient.java
+++ src/main/java/com/book_shop/bookstore/order/domain/Recipient.java
@@ -1,24 +1,17 @@
 package com.book_shop.bookstore.order.domain;
 
-import lombok.Builder;
-import lombok.Value;
+import lombok.*;
 
-@Value
+@Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Recipient {
-    String name;
-    String phone;
-    String street;
-    String city;
-    String zipCode;
-    String email;
+    private String name;
+    private String phone;
+    private String street;
+    private String city;
+    private String zipCode;
+    private String email;
 
-    public Recipient(String name, String phone, String street, String city, String zipCode, String email) {
-        this.name = name;
-        this.phone = phone;
-        this.street = street;
-        this.city = city;
-        this.zipCode = zipCode;
-        this.email = email;
-    }
 }
