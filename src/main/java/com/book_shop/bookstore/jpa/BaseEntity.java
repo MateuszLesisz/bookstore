c5838c1e5411aacabf86ec7d07401b822c81569e
Mateusz Lesisz <m.lesisz97@wp.pl>
2023-11-28 15:34:07 +0100

    add optimistic locking

--- src/main/java/com/book_shop/bookstore/jpa/BaseEntity.java
+++ src/main/java/com/book_shop/bookstore/jpa/BaseEntity.java
@@ -3,6 +3,7 @@ package com.book_shop.bookstore.jpa;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,11 +13,14 @@ import java.util.UUID;
 @MappedSuperclass
 @Getter
 @Setter
-@EqualsAndHashCode
+@EqualsAndHashCode(of = "uuid")
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue
     private Long id;
     private String uuid = UUID.randomUUID().toString();
+
+    @Version
+    private long version;
 }
