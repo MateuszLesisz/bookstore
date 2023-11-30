18b3c517e98d14e86c8c99682ebea6f248e59cef
Mateusz <88348890+MateuszLesisz@users.noreply.github.com>
2023-11-30 16:40:10 +0100

    add only jpa layer test

---
+++ src/test/java/com/book_shop/bookstore/order/application/ManipulateOrderServiceTest.java
@@ -0,0 +1,56 @@
+package com.book_shop.bookstore.order.application;
+
+import com.book_shop.bookstore.catalog.db.BookJpaRepository;
+import com.book_shop.bookstore.catalog.domain.Book;
+import com.book_shop.bookstore.order.application.port.ManipulateOrderUseCase.OrderItemCommand;
+import com.book_shop.bookstore.order.application.port.ManipulateOrderUseCase.PlaceOrderCommand;
+import com.book_shop.bookstore.order.application.port.ManipulateOrderUseCase.PlaceOrderResponse;
+import com.book_shop.bookstore.order.domain.Recipient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Import({ManipulateOrderService.class})
+class ManipulateOrderServiceTest {
+
+    @Autowired
+    BookJpaRepository bookRepository;
+    @Autowired
+    ManipulateOrderService orderService;
+    @Test
+    public void userCanPlaceOrder() {
+        //given
+        Book effectiveJava = givenEffectiveJava(50L);
+        Book jcip = givenJavaConcurrency(50L);
+        PlaceOrderCommand command = PlaceOrderCommand
+                .builder()
+                .recipient(recipient())
+                .orderItem(new OrderItemCommand(effectiveJava.getId(), 10))
+                .orderItem(new OrderItemCommand(jcip.getId(), 10))
+                .build();
+
+        //when
+        PlaceOrderResponse response = orderService.placeOrder(command);
+
+        //then
+        assertTrue(response.isSuccess());
+    }
+
+    private Book givenJavaConcurrency(long available) {
+        return bookRepository.save(new Book("Java concurrency in Practice", 2006, new BigDecimal("99.90"), available));
+    }
+
+    private Book givenEffectiveJava(long available) {
+        return bookRepository.save(new Book("Effective Java", 2005, new BigDecimal("199.90"), available));
+    }
+
+    private Recipient recipient() {
+        return Recipient.builder().email("john@example.org").build();
+    }
+}
