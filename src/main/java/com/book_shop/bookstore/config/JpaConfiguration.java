468a224b214cf2fa320b9b137dabde8e136bf659
Mateusz <88348890+MateuszLesisz@users.noreply.github.com>
2023-12-16 12:45:56 +0100

    add my own defaultSecurityFilterChain method

--- src/main/java/com/book_shop/bookstore/config/JpaConfiguration.java
+++ src/main/java/com/book_shop/bookstore/config/JpaConfiguration.java
@@ -1,4 +1,4 @@
-package com.book_shop.bookstore;
+package com.book_shop.bookstore.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
---
+++ src/main/java/com/book_shop/bookstore/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.book_shop.bookstore.config;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    @SneakyThrows
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
