58cd380ff564894f0c92b13d340059f1bc63653b
Mateusz <88348890+MateuszLesisz@users.noreply.github.com>
2023-12-18 14:34:37 +0100

    use aws db to authenticate

--- src/main/java/com/book_shop/bookstore/config/SecurityConfig.java
+++ src/main/java/com/book_shop/bookstore/config/SecurityConfig.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -28,21 +28,12 @@ public class SecurityConfig {
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-
-        UserDetails admin = User.withUsername("admin")
-                .password("{noop}pass123")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password("{noop}12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/com/book_shop/bookstore/jpa/BaseEntity.java
+++ src/main/java/com/book_shop/bookstore/jpa/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.book_shop.bookstore.jpa;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,7 +14,7 @@ import java.util.UUID;
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String uuid = UUID.randomUUID().toString();
     @Version
