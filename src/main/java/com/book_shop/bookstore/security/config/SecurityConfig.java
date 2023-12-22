8f0ea865acb13ea94231159892097a8cdcd11729
Mateusz <88348890+MateuszLesisz@users.noreply.github.com>
2023-12-22 21:42:59 +0100

    fix cors issue

--- src/main/java/com/book_shop/bookstore/security/config/SecurityConfig.java
+++ src/main/java/com/book_shop/bookstore/security/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.book_shop.bookstore.security.config;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,6 +10,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
@@ -18,11 +20,19 @@ public class SecurityConfig {
     @Bean
     @SneakyThrows
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.cors(cors -> cors.configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowCredentials(true);
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setMaxAge(3600L);
+            return config;
+        }));
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(requests -> {
-            requests.requestMatchers("/catalog/**", "/admin/**", "/orders/**", "/uploads/**", "/users/**").authenticated()
-                    .requestMatchers("/authors/**", "/register/**").permitAll();
-        });
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/catalog/**", "/admin/**", "/orders/**", "/uploads/**", "/users/**").authenticated()
+                        .requestMatchers("/authors/**", "/register/**").permitAll());
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
