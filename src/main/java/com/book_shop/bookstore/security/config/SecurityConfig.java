ed5ae34ff60ae20c375f9244b7117751987494b5
Mateusz <88348890+MateuszLesisz@users.noreply.github.com>
2023-12-27 16:24:19 +0100

    add csrf token

--- src/main/java/com/book_shop/bookstore/security/config/SecurityConfig.java
+++ src/main/java/com/book_shop/bookstore/security/config/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.book_shop.bookstore.security.config;
 
+import com.book_shop.bookstore.filter.CsrfCookieFilter;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
@@ -17,10 +21,16 @@ import java.util.Collections;
 @Configuration
 public class SecurityConfig {
 
+    private static final String REGISTER_PATH = "/register";
+
     @Bean
     @SneakyThrows
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
-        http.cors(cors -> cors.configurationSource(request -> {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+
+        http.securityContext(context -> context.requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(cors -> cors.configurationSource(request -> {
             CorsConfiguration config = new CorsConfiguration();
             config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
             config.setAllowedMethods(Collections.singletonList("*"));
@@ -29,10 +39,12 @@ public class SecurityConfig {
             config.setMaxAge(3600L);
             return config;
         }));
-        http.csrf(csrf -> csrf.ignoringRequestMatchers("/register"))
+        http.csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers(REGISTER_PATH)
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/catalog/**", "/admin/**", "/orders/**", "/uploads/**", "/users/**").authenticated()
-                        .requestMatchers("/authors/**", "/register").permitAll());
+                        .requestMatchers("/authors/**", REGISTER_PATH).permitAll());
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
